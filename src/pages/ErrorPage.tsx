43e3bf35c32cbe3094a5aa27cc30fc6c5d803115
Lukas Pisarcik <lukas.pisarcik@telekom.com>
2024-03-13 21:41:14 +0100

    error fix

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -8,9 +8,6 @@ export default function ErrorPage() {
     <div id="error-page" className="p-4 mt-10 w-11/12 flex text-center m-auto gap-5 flex-col">
       <h1 className="scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-6xl">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
     </div>
   );
 }
