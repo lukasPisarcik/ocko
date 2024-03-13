0fa54daded034e61042dcde40345f34bd0be9d0f
Lukas Pisarcik <lukas.pisarcik@telekom.com>
2024-03-13 21:39:20 +0100

    first working version

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,44 +1,61 @@
-import Player from "@/components/Player";
+import React from 'react';
+import PlayerCard from "@/components/PlayerCard";
 import { Progress } from "@/components/ui/progress"
-import { getCurrentGame } from "@/uttlis";
+import { getCurrentGame, saveToLocalStorage } from '@/uttlis';
 import { Game } from "@/models/Game";
+import { Player } from "@/models/Player";
 
 const GamePage = () => {
+    const [gameConfig, setGameConfig] = React.useState<Game | null>(getCurrentGame());
 
-    const gameConfig: Game | null = getCurrentGame()
+    // Function to update player value
+    const updatePlayerValue = (index: number, newValue: number) => {
+      if (gameConfig) {
+          const updatedPlayers = [...gameConfig.players];
+          updatedPlayers[index].value = newValue;
+          const playersValueSum = updatedPlayers.reduce((sum, player) => sum + (player.value * -1), 0);
+          const newCurrentBank = gameConfig.bank + playersValueSum;
+          setGameConfig({ ...gameConfig, players: updatedPlayers, currentBank: newCurrentBank });
+          saveToLocalStorage(gameConfig);
+      }
+  };
 
     return (
-      <>
-        { gameConfig ? 
-        <div className="p-4 mt-10 w-11/12 flex justify-center align-middle m-auto gap-10 flex-col">
-          <div className="flex w-full justify-between items-center">
-            <div className="flex gap-1">
-              <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">
-                Bank      
-              </h1>
-              <h1 className="scroll-m-20 text-4xl tracking-tight text-muted-foreground">
-                {gameConfig.bank}€      
-              </h1>
-            </div>
-            <div className="self-center text-center">
-              <p className="text-sm font-bold text-muted-foreground">Zostatok</p>
-              <h1 className="scroll-m-20 text-4xl tracking-tight text-muted-foreground text-emerald-500">
-              {gameConfig.currentBank}€     
-              </h1>
-            </div>
-          </div>
+        <>
+            {gameConfig ? (
+                <div className="p-4 mt-10 w-11/12 flex justify-center align-middle m-auto gap-10 flex-col">
+                    <div className="flex w-full justify-between items-center">
+                        <div className="flex gap-1">
+                            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">
+                                Bank
+                            </h1>
+                            <h1 className="scroll-m-20 text-4xl tracking-tight text-muted-foreground">
+                                {gameConfig.bank}€
+                            </h1>
+                        </div>
+                        <div className="self-center text-center">
+                            <p className="text-sm font-bold text-muted-foreground">Zostatok</p>
+                            <h1 className="scroll-m-20 text-4xl tracking-tight text-muted-foreground text-emerald-500">
+                                {gameConfig.currentBank}€
+                            </h1>
+                        </div>
+                    </div>
 
-          <Progress value={(gameConfig.currentBank/gameConfig.bank)*100} />
+                    <Progress value={(gameConfig.currentBank / gameConfig.bank) * 100} />
 
-          {gameConfig.players.map((player, index) => (
-            <Player key={index} playerName={player.name} />
-          ))}
-        </div>
-        : 
-          <p>Zly game config</p>
-        }
-      </>
-    )
-  };
-  
-  export default GamePage;
+                    {gameConfig.players.map((player: Player, index: number) => (
+                        <PlayerCard
+                            key={index}
+                            player={player}
+                            onUpdatePlayer={(newValue: number) => updatePlayerValue(index, newValue)}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <p>Zly game config</p>
+            )}
+        </>
+    );
+};
+
+export default GamePage;
